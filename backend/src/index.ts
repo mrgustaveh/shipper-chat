04089a481d240ae70b62d3ba1a73a9895b87170c
mrgustaveh <mburuantony@outlook.com>
2026-02-24 09:50:15 +0300

    fix/backend: sockets

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
-dotenv.config();
 import http from "http";
 import { Server } from "socket.io";
 import app from "./app";
 import { setupChatSockets } from "./sockets/chat";
 import { setupPresenceSockets } from "./sockets/presence";
 import { socketAuthMiddleware } from "./sockets/authMiddleware";
+
+dotenv.config();
 
 const port = process.env.PORT || 8001;
 const server = http.createServer(app);
