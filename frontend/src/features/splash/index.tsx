d4789fec098f080ca7e4ea7a010f83cd17de846a
mrgustaveh <mburuantony@outlook.com>
2026-02-25 18:59:59 +0300

    update: landing page

--- frontend/src/features/splash/index.tsx
+++ frontend/src/features/splash/index.tsx
@@ -4,18 +4,12 @@ import { dark } from "@clerk/themes";
 import { useNavigate } from "react-router";
 import { useDisclosure } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
+import { Divider } from "@mantine/core";
 import { FiChevronsRight } from "react-icons/fi";
-import {
-  IoChatboxEllipses,
-  IoChatboxEllipsesOutline,
-  IoChatbubbles,
-  IoChatbubblesOutline,
-  IoChatbubbleEllipsesOutline,
-  IoChatbubbleEllipsesSharp,
-} from "react-icons/io5";
 import { useApiAuth } from "@/hooks/data/useapiauth";
 import { UiModal } from "@/components/ui/UiModal";
 import { Loading } from "@/assets/animations";
+import { SquigglyWave } from "@/assets/Illustrations";
 import "./index.scss";
 
 export default function Splash() {
@@ -70,39 +64,41 @@ export default function Splash() {
 
   return (
     <section id="splash">
-      <div id="els" className="els_above">
-        <span className="els_icon_ctr">
-          <IoChatbubbles className="els_icon" fontSize={32} />
-        </span>
-
-        <span className="els_icon_ctr mid">
-          <IoChatboxEllipsesOutline className="els_icon" fontSize={32} />
-        </span>
-
-        <span className="els_icon_ctr">
-          <IoChatboxEllipses className="els_icon" fontSize={32} />
-        </span>
+      <div id="animation_ctr">
+        <SquigglyWave />
       </div>
 
-      <button id="signin" onClick={onSignIn}>
-        Get Started
-        <span className="icon_ctr">
-          <FiChevronsRight className="icon" fontSize={18} />
-        </span>
-      </button>
+      <div id="contents">
+        <div className="intro">
+          <img src="/icon-bg.png" alt="icon" />
+          <span>♥️ Loved by 1M+ People</span>
+        </div>
+        <p className="intro_name">
+          Introducing <span>Shipper - Chat</span>, <br /> The <span>Best</span>{" "}
+          way to <span>Chat.</span>
+        </p>
+        <button id="signin" onClick={onSignIn}>
+          Get Started
+          <span className="icon_ctr">
+            <FiChevronsRight className="icon" fontSize={18} />
+          </span>
+        </button>
 
-      <div id="els" className="els_below">
-        <span className="els_icon_ctr">
-          <IoChatbubbleEllipsesOutline className="els_icon" fontSize={32} />
-        </span>
+        <div className="previews">
+          {preview_avatars.map((_avatar, idx) => (
+            <div className="skeleton_ctr" key={idx}>
+              <div className="img_uname">
+                <img src={_avatar.image} alt="avatar" />
+                <span className="user_name">@{_avatar.name}</span>
+              </div>
 
-        <span className="els_icon_ctr mid">
-          <IoChatbubbleEllipsesSharp className="els_icon" fontSize={32} />
-        </span>
+              <Divider style={{ margin: "0.5rem 0" }} />
 
-        <span className="els_icon_ctr">
-          <IoChatbubblesOutline className="els_icon" fontSize={32} />
-        </span>
+              <p className="msg">Lorem ipsum dolor sit...</p>
+              <span className="time">2 mins ago</span>
+            </div>
+          ))}
+        </div>
       </div>
 
       <UiModal
@@ -123,3 +119,31 @@ export default function Splash() {
     </section>
   );
 }
+
+type preview = {
+  name: string;
+  image: string;
+};
+
+const preview_avatars: preview[] = [
+  {
+    name: "oliver",
+    image: "https://api.dicebear.com/9.x/adventurer-neutral/svg?seed=Oliver",
+  },
+  {
+    name: "ryan",
+    image: "https://api.dicebear.com/9.x/adventurer/svg?seed=Ryan",
+  },
+  {
+    name: "george",
+    image: "https://api.dicebear.com/9.x/open-peeps/svg?seed=George",
+  },
+  {
+    name: "aiden",
+    image: "https://api.dicebear.com/9.x/open-peeps/svg?seed=Oliver",
+  },
+  {
+    name: "valentina",
+    image: "https://api.dicebear.com/9.x/toon-head/svg?seed=Valentina",
+  },
+];
