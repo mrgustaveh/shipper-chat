3c503915c83a344c26a8f3907f049e1322e35dd7
mrgustaveh <mburuantony@outlook.com>
2026-02-26 02:15:47 +0300

    fix: environment variables mismatch

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -6,9 +6,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY || process.env.API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET || process.env.API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
